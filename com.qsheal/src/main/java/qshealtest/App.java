573a5ebe7f52dedcdb6afa60e22447cacc5b6596
yz190598886 <190598886@qq.com>
2018-01-04 18:29:19 +0800

    Signed-off-by: yz190598886 <190598886@qq.com>

---
+++ com.qsheal/src/main/java/qshealtest/App.java
@@ -0,0 +1,13 @@
+package qshealtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- com.qsheal/src/test/java/demo/Dapeican.java
+++ com.qsheal/src/test/java/demo/Dapeican.java
@@ -1,14 +1,22 @@
 package demo;
-import org.junit.Test;
+import org.junit.*;
+
 public class Dapeican {
+    @Before
+    public void Start(){
+        System.out.println("开始-------------------------");
+    }
     @Test
     public void login() {
         TestPost ok = new TestPost();
         try {
-            ok.getResoucesByLoginCookies();
+             ok.getResoucesByLoginCookies();
         } catch (Exception e) {
             e.printStackTrace();
-
         }
+    }
+    @After
+    public void Over (){
+        System.out.println("结束-------------------------");
     }
 }
