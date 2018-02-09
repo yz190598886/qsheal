271f2957450b59ab911613bf7a8830658d1d71a9
yz190598886 <190598886@qq.com>
2018-02-09 17:56:01 +0800

    Signed-off-by: yz190598886 <190598886@qq.com>

---
+++ com.qsheal/src/test/java/demo/AddUser.java
@@ -0,0 +1,19 @@
+package demo;
+
+import TestCase.XueyuanAdd;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class AddUser {
+    @Test
+    public void one() throws IOException {
+        XueyuanAdd xueyuanAdd =new XueyuanAdd();
+            xueyuanAdd.addUser();
+    }
+public void two(){
+
+}
+}
+
+
